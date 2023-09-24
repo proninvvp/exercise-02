300eafc3509889336d39394d0272ff6ebf9f683d
Valerii Pronin <proninvvp@gmail.com>
2023-09-24 16:31:30 +0300

    Ошибка в адресе

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 
 if (isset($_SESSION['userName'])) {
     echo 'Привет ' . $_SESSION['userName'];
-    echo   "<br><a href='/exit.php'>Выход</a>";
+    echo   "<br><a href='exit.php'>Выход</a>";
 } else {
     include 'web/index.php';
 }
